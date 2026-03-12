d579c92410b929aa0b7479deb956028e267a1106
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-03-12 12:15:27 +0800

    Menambahkan deklarasi array arr[20] dan variabel n

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int arr[20];        //membuat array dengan panjang data 20
+int n;              //membuat variable inputan n
+
+void input (){ //procedure input
+    while (true){
+        cout << "masukkan jumlah data pada array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\narray ridak boleh lebih dari 20\n";
+        }
+    }
+    cout << endl;
+}
